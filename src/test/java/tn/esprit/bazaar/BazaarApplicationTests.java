bb349063219e67ad4e658ed543def8c4e9feaaaf
syrineahmed <syrine.ahmed@esprit.tn>
2024-07-14 17:35:40 +0200

    firstpush

---
+++ src/test/java/tn/esprit/bazaar/BazaarApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.bazaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
